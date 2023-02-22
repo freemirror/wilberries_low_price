d5f943c86eca95496e012f84020c23baf8bec410
freemirror <freemirror@ya.ru>
2023-02-22 15:39:55 +0400

    stable v0.4 modify functions

--- parcer_test.py
+++ parcer_test.py
@@ -5,6 +5,24 @@ import time
 
 
 CATALOG_ID_EXCEPTION = [2192, 130255, 61037, 4853, 12, 13, 14]
+HEADERS = {'Accept': "*/*", 'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
+
+
+def get_data_from_wb(url):
+    retry_counter = 0
+    while retry_counter < 5:
+        try:
+            response = requests.get(url, headers=HEADERS)
+            break
+        except:
+            time.sleep(3)
+            retry_counter += 1
+            logger.error(f'Ошибка доступа к ресурсу, попытка подключения №{retry_counter}')
+            return None
+    if response.status_code != 200:
+        logger.error(f'Данные не загружены, сервер вернул код {response.status_code}')
+        return None
+    return response
 
 
 def get_child_catalog_wb(catalogs, catalogs_list):
@@ -24,48 +42,39 @@ def get_child_catalog_wb(catalogs, catalogs_list):
 
 
 def get_catalog_wb():
+    logger.info('Загрузка списка каталогов')
     url = "https://static-basket-01.wb.ru/vol0/data/main-menu-ru-ru-v2.json"
-    headers = {'Accept': "*/*", 'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
     catalogs_list = []
-    response = requests.get(url, headers=headers)
-    try:
-        data = response.json()
-        catalogs_list = get_child_catalog_wb(data, catalogs_list)
-    except:
-        logging.error('Не удалось загрузить список каталогов')
+    response = get_data_from_wb(url)
+    if not response:
+        logger.info('Список каталогов не загружен')
+        return catalogs_list
+    data = response.json()
+    catalogs_list = get_child_catalog_wb(data, catalogs_list)
+    logger.info('Список каталогов загружен')
     return catalogs_list
 
 
 def get_content(shard, query, low_price=0, top_price=0):
-    headers = {'Accept': "*/*", 'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
     data_list = []
     for page in range(1, 101):
         print(f'Сбор позиций со страницы {page} из 100')
-        retry_counter = 0
         url = f'https://catalog.wb.ru/catalog/{shard}/catalog?appType=1&curr=rub&dest=-1075831,-77677,-398551,12358499' \
               f'&locale=ru&page={page}&price={low_price * 100};{top_price * 100}' \
               f'®=0®ions=64,83,4,38,80,33,70,82,86,30,69,1,48,22,66,31,40&sort=popular&spp=0&{query}'
-        while retry_counter < 5:
-            try:
-                response = requests.get(url, headers=headers)
-                retry_counter = 6
-            except:
-                time.sleep(3)
-                retry_counter += 1
-                logging.error(f'Ошибка доступа к ресурсу, попытка №{retry_counter}')
-            else:
-                if response.status_code == 200:
-                    data = response.json()
-                    products = data['data']['products']
-                    if not products:
-                        print(f'Сбор данных завершен на странице {page - 1}')
-                        break
-                    data_list.extend(get_data_from_json(products))
-                else:
-                    logging.error(f'Ошибка ответа сервера {response.status_code}')
-        if retry_counter == 5:
-            logging.info(f'Страница {page} не загружена')
-    print(f'Собрана информация по {len(data_list)} позиций')
+        response = get_data_from_wb(url)
+        if not response:
+            logger.info(f'Страница {page} не загружена')
+            continue
+        
+        data = response.json()
+        products = data['data']['products']
+        if not products:
+            logger.info(f'Сбор данных завершен на странице {page - 1}')
+            break
+        data_list.extend(get_data_from_json(products))
+
+    logger.info(f'Собрана информация по {len(data_list)} позиций')
     return data_list
 
 
@@ -87,20 +96,23 @@ def get_data_from_json(products):
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, filename="parcer_log.log")
+    logger= logging.getLogger()
+    logger.setLevel(logging.INFO) 
+    handler = logging.FileHandler('parcer_log.log', 'w', 'utf-8') 
+    formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s') 
+    handler.setFormatter(formatter) 
+    logger.addHandler(handler)
+
     catalogs = get_catalog_wb()
+
     if catalogs:
-        # with open('wb_catalogs_data.json', 'w', encoding='UTF-8') as file:
-        #     json.dump(catalogs, file, indent=2, ensure_ascii=False)
-        #     print(f'Данные каталогов сохранены в wb_catalogs_data.json')
-        """Раскоментировать если нужно сохранить список каталогов"""
+        with open('wb_catalogs_data.json', 'w', encoding='UTF-8') as file:
+            json.dump(catalogs, file, indent=2, ensure_ascii=False)
         for catalog in catalogs:
             name = catalog['name']
-            logging.info(f'Загрузка каталога {name}')
-            print(f'Загрузка данных из каталога {name}')
+            logger.info(f'Загрузка товаров из каталога: {name}')
+            print(f'Загрузка товаров из каталога: {name}')
             result = get_content(catalog['shard'], catalog['query'])
             with open('wb_goods_data.json', 'a', encoding='UTF-8') as file:
                 json.dump(result, file, indent=2, ensure_ascii=False)
-                print(f'Данные раздела {name} сохранены в wb_goods_data.json')
-    else:
-        print('Не получилось загрузить список каталогов')
+                logger.info(f'Данные каталога {name} сохранены')
